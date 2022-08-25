72c1f0550f11f0975375dd052b82b5b45c601a9c
Jonatas Miguel <jonatas_miguel@hotmail.com>
2022-08-25 06:04:53 -0300

    Update server.py

--- microservices/binary_executor_image/server.py
+++ microservices/binary_executor_image/server.py
@@ -4,6 +4,9 @@ from binary_execution import Execution, Parameters
 from utils import UserRequest, Database, ObjectStorage, Data, Metadata
 from typing import Union
 from constants import Constants
+import logging
+
+logging.basicConfig(filename='myapp.log', level=logging.INFO)
 
 app = Flask(__name__)
 
@@ -22,6 +25,7 @@ parameters_handler = Parameters(database, data)
 
 @app.route(Constants.MICROSERVICE_URI_PATH, methods=["POST"])
 def create_execution() -> jsonify:
+    logging.info('Start create_execution')
     service_type = request.args.get(Constants.TYPE_FIELD_NAME)
 
     model_name = request.json[Constants.MODEL_NAME_FIELD_NAME]
@@ -62,6 +66,7 @@ def create_execution() -> jsonify:
     train_model.create(
         module_path, class_name, method_parameters, description)
 
+    logging.info('Finish create_execution')
     return (
         jsonify({
             Constants.MESSAGE_RESULT:
@@ -73,6 +78,7 @@ def create_execution() -> jsonify:
 
 @app.route(f'{Constants.MICROSERVICE_URI_PATH}/<filename>', methods=["PATCH"])
 def update_execution(filename: str) -> jsonify:
+    logging.info('Start update_execution')
     service_type = request.args.get(Constants.TYPE_FIELD_NAME)
     description = request.json[Constants.DESCRIPTION_FIELD_NAME]
     method_parameters = request.json[Constants.METHOD_PARAMETERS_FIELD_NAME]
@@ -109,6 +115,8 @@ def update_execution(filename: str) -> jsonify:
     default_model.update(
         module_path, method_parameters, description)
 
+    logging.info('Finish create_execution')
+
     return (
         jsonify({
             Constants.MESSAGE_RESULT:
@@ -120,6 +128,7 @@ def update_execution(filename: str) -> jsonify:
 
 @app.route(f'{Constants.MICROSERVICE_URI_PATH}/<filename>', methods=["DELETE"])
 def delete_default_model(filename: str) -> jsonify:
+    logging.info('Start delete_default_model')
     service_type = request.args.get(Constants.TYPE_FIELD_NAME)
 
     try:
@@ -134,6 +143,8 @@ def delete_default_model(filename: str) -> jsonify:
         )
 
     storage.delete(filename, service_type)
+
+    logging.info('Finish create_execution')
 
     return (
         jsonify({
@@ -150,40 +161,48 @@ def analyse_post_request_errors(request_validator: UserRequest,
                                 class_method: str,
                                 method_parameters: dict) \
         -> Union[tuple, None]:
+    logging.info('Start analyse_post_request_errors')    
+    logging.info('      Start not_duplicated_filename_validator') 
     try:
         request_validator.not_duplicated_filename_validator(
             filename
         )
     except Exception as duplicated_train_filename:
+        logging.warn(f'Exception: {str(duplicated_train_filename)}')    
         return (
             jsonify({Constants.MESSAGE_RESULT: str(duplicated_train_filename)}),
             Constants.HTTP_STATUS_CODE_CONFLICT,
         )
 
+    logging.info('      Start existent_filename_validator => parent_name') 
     try:
         request_validator.existent_filename_validator(
             parent_name
         )
     except Exception as invalid_parent_name:
+        logging.warn(f'Exception: {str(invalid_parent_name)}')
         return (
             jsonify({Constants.MESSAGE_RESULT: str(invalid_parent_name)}),
             Constants.HTTP_STATUS_CODE_NOT_ACCEPTABLE,
         )
 
+    logging.info('      Start existent_filename_validator => model_name') 
     try:
         request_validator.existent_filename_validator(
             model_name
         )
     except Exception as invalid_parent_name:
+        logging.warn(f'Exception: {str(invalid_parent_name)}')
         return (
             jsonify({Constants.MESSAGE_RESULT: str(invalid_parent_name)}),
             Constants.HTTP_STATUS_CODE_NOT_ACCEPTABLE,
         )
 
-
+    logging.info('      Start get_module_and_class_from_a_instance') 
     module_path, class_name = data.get_module_and_class_from_a_instance(
         model_name)
 
+    logging.info('      Start valid_method_class_validator') 
     try:
         request_validator.valid_method_class_validator(
             module_path,
@@ -191,11 +210,13 @@ def analyse_post_request_errors(request_validator: UserRequest,
             class_method
         )
     except Exception as invalid_method_name:
+        logging.warn(f'Exception: {str(invalid_method_name)}')
         return (
             jsonify({Constants.MESSAGE_RESULT: str(invalid_method_name)}),
             Constants.HTTP_STATUS_CODE_NOT_ACCEPTABLE,
         )
 
+    logging.info('      Start valid_method_parameters_validator') 
     try:
         request_validator.valid_method_parameters_validator(
             module_path,
@@ -204,12 +225,13 @@ def analyse_post_request_errors(request_validator: UserRequest,
             method_parameters
         )
     except Exception as invalid_method_parameters:
+        logging.warn(f'Exception: {str(invalid_method_parameters)}')
         return (
             jsonify({Constants.MESSAGE_RESULT: str(invalid_method_parameters)}),
             Constants.HTTP_STATUS_CODE_NOT_ACCEPTABLE,
         )
 
-
+    logging.info('Finish create_execution')
     return None
 
 
@@ -219,21 +241,27 @@ def analyse_patch_request_errors(request_validator: UserRequest,
                                  model_name: str,
                                  method_parameters: dict) \
         -> Union[tuple, None]:
+    logging.info('Start analyse_patch_request_errors') 
+
+    logging.info('      Start existent_filename_validator') 
     try:
         request_validator.existent_filename_validator(
             filename
         )
     except Exception as nonexistent_train_filename:
+        logging.warn(f'Exception: {str(nonexistent_train_filename)}')
         return (
             jsonify(
                 {Constants.MESSAGE_RESULT: str(nonexistent_train_filename)}),
             Constants.HTTP_STATUS_CODE_NOT_FOUND,
         )
 
+    logging.info('      Start get_module_and_class_from_a_executor_name')
     module_path, class_name = data.get_module_and_class_from_a_executor_name(
         model_name)
-
+    logging.info('      Start get_class_method_from_a_executor_name')
     class_method = data.get_class_method_from_a_executor_name(filename)
+    logging.info('      Start valid_method_parameters_validator')
     try:
         request_validator.valid_method_parameters_validator(
             module_path,
@@ -242,12 +270,13 @@ def analyse_patch_request_errors(request_validator: UserRequest,
             method_parameters
         )
     except Exception as invalid_function_parameters:
+        logging.warn(f'Exception: {str(invalid_function_parameters)}')
         return (
             jsonify(
                 {Constants.MESSAGE_RESULT: str(invalid_function_parameters)}),
             Constants.HTTP_STATUS_CODE_NOT_ACCEPTABLE,
         )
-
+    logging.info('Finish analyse_patch_request_errors')
     return None
 
 
